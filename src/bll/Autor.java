45bd2d8924b25e13ae70e8478d324a028d80da23
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 20:39:54 -0300

    feat: crear entidad Autor

---
+++ src/bll/Autor.java
@@ -0,0 +1,43 @@
+package bll;
+
+public class Autor {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+
+    public Autor() {
+    }
+
+    public Autor(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor: " +
+                "\nNombre: " + nombre +
+                "\nApellido: " + apellido;
+    }
+}

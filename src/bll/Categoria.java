eac632f4dc4dc9affa50522a8315fa5d0ae3bff4
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 20:40:53 -0300

    feat: crear entidad Categoria

---
+++ src/bll/Categoria.java
@@ -0,0 +1,33 @@
+package bll;
+
+public class Categoria {
+
+    private int id;
+    private String nombre;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoría: " +
+                "\nNombre: " + nombre;
+    }
+
+}

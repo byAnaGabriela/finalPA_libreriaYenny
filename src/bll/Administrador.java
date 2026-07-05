6f59fb354ef1fabf4927bbb1fde1df7314e9a740
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 20:04:56 -0300

    feat: crear entidad Administrador
    Clase hija de Usuario, con Rol.ADMINISTRADOR

---
+++ src/bll/Administrador.java
@@ -0,0 +1,17 @@
+package bll;
+
+import enums.EstadoUsuario;
+import enums.Rol;
+
+import java.time.LocalDateTime;
+
+public class Administrador extends Usuario {
+
+    public Administrador() {
+    }
+
+    public Administrador(String nombre, String apellido, String dni, String celular, String mail, String nombreUsuario, String contrasena, LocalDateTime fechaRegistro, EstadoUsuario estado) {
+        super(Rol.ADMINISTRADOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
+    }
+
+}

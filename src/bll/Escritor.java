b14a4dab14627c3842cbc55347a4a22fc0e24ec8
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 20:05:59 -0300

    feat: crear entidad Escritor
    Clase hija de Usuario, con Rol.ESCRITOR

---
+++ src/bll/Escritor.java
@@ -0,0 +1,17 @@
+package bll;
+
+import enums.EstadoUsuario;
+import enums.Rol;
+
+import java.time.LocalDateTime;
+
+public class Escritor extends Usuario {
+
+    public Escritor() {
+    }
+
+    public Escritor(String nombre, String apellido, String dni, String celular, String mail, String nombreUsuario, String contrasena, LocalDateTime fechaRegistro, EstadoUsuario estado) {
+        super(Rol.ESCRITOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
+    }
+
+}

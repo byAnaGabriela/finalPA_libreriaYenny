399b75bbc6698a83c643949aa8be4def0983904a
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 20:05:33 -0300

    feat: crear entidad Editor
    Clase hija de Usuario, con Rol.EDITOR

---
+++ src/bll/Editor.java
@@ -0,0 +1,17 @@
+package bll;
+
+import enums.EstadoUsuario;
+import enums.Rol;
+
+import java.time.LocalDateTime;
+
+public class Editor extends Usuario {
+
+    public Editor() {
+    }
+
+    public Editor(String nombre, String apellido, String dni, String celular, String mail, String nombreUsuario, String contrasena, LocalDateTime fechaRegistro, EstadoUsuario estado) {
+        super(Rol.EDITOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
+    }
+
+}

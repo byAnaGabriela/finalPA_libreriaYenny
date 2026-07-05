e96894459a525cde3c171e7d2c50aed25c365964
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 19:38:54 -0300

    feat: crear entidad Usuario
    Definición de atributos y métodos base

---
+++ src/bll/Usuario.java
@@ -0,0 +1,140 @@
+package bll;
+
+import enums.Rol;
+import enums.EstadoUsuario;
+
+import java.time.LocalDateTime;
+
+public abstract class Usuario {
+
+    private String id;
+    private Rol rol;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String celular;
+    private String mail;
+    private String nombreUsuario;
+    private String contrasena;
+    private LocalDateTime fechaRegistro;
+    private EstadoUsuario estado;
+
+    public Usuario() {
+    }
+
+    public Usuario(Rol rol, String nombre, String apellido, String dni, String celular, String mail, String nombreUsuario, String contrasena, LocalDateTime fechaRegistro, EstadoUsuario estado) {
+        this.rol = rol;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.celular = celular;
+        this.mail = mail;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.fechaRegistro = fechaRegistro;
+        this.estado = estado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public EstadoUsuario getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoUsuario estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario:" +
+                "\nRol: " + rol +
+                "\nNombre: '" + nombre +
+                "\nApellido: '" + apellido +
+                "\nDni: '" + dni +
+                "\nCelular: '" + celular +
+                "\nMail: '" + mail+
+                "\nNombre de usuario: '" + nombreUsuario +
+                "\nContraseña: '" + contrasena +
+                "\nFecha de registro: " + fechaRegistro +
+                "\nEstado=" + estado;
+    }
+}

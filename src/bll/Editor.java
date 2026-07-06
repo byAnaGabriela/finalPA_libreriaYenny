6256de5ee1627e9ad7a20b72c6f4dba6461de3cf
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 22:29:25 -0300

    refactor(editor): agregar constructor con todos los atributos

--- src/bll/Editor.java
+++ src/bll/Editor.java
@@ -14,4 +14,7 @@ public class Editor extends Usuario {
         super(Rol.EDITOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
     }
 
+    public Editor(int id, Rol rol, String nombre, String apellido, String dni, String celular, String mail, String nombreUsuario, String contrasena, LocalDateTime fechaRegistro, EstadoUsuario estado) {
+        super(id, Rol.EDITOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
+    }
 }

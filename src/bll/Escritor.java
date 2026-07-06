b5233ac6ca1013e32c7342f47fafa79809c88e56
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-05 22:32:27 -0300

    refactor(escritor): agregar constructor con todos los atributos

--- src/bll/Escritor.java
+++ src/bll/Escritor.java
@@ -14,4 +14,8 @@ public class Escritor extends Usuario {
         super(Rol.ESCRITOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
     }
 
+    public Escritor(int id, Rol rol, String nombre, String apellido, String dni, String celular, String mail, String nombreUsuario, String contrasena, LocalDateTime fechaRegistro, EstadoUsuario estado) {
+        super(id, Rol.ESCRITOR, nombre, apellido, dni, celular, mail, nombreUsuario, contrasena, fechaRegistro, estado);
+    }
+
 }

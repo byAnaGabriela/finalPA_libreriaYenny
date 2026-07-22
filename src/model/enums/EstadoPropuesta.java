69a52f24332a88d6105106f90d88771dea197a90
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-21 22:06:24 -0300

    refactor(enums): reemplazar la clase por un enum y reorganizar su ubicación
    - La clase EstadoPropuesta ahora es un enum
    - Se movio a el paquete enums que está dentro de model

---
+++ src/model/enums/EstadoPropuesta.java
@@ -0,0 +1,11 @@
+package model.enums;
+
+public enum EstadoPropuesta {
+    RECIBIDA,
+    EN_EVALUACION,
+    APROBADA_PARA_EDICION,
+    RECHAZADA,
+    REQUIERE_MODIFICACIONES,
+    LISTA_PARA_IMPRESION,
+    PUBLICADA
+}

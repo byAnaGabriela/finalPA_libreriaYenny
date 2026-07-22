50110c9947eb80af245fd60cc94e53fd4fdefacb
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-21 22:37:40 -0300

    refactor(libro): reorganiza la clase y amplía funcionalidad
    - Se movió la clase a un nuevo paquete.
    - Se agregaron nuevos atributos.
    - Se incorporaron métodos booleanos para validar estados.

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -1,7 +1,7 @@
-package bll;
+package model;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Libro {
@@ -11,33 +11,39 @@ public class Libro {
     private String titulo;
     private String sinopsis;
     private int cantidadPaginas;
-    private double precio;
-    private LocalDateTime fechaPublicacion;
+    private BigDecimal precio;
+    private LocalDate fechaPublicacion;
+    private int cantidadDisponible;
+    private int stockMinimo;
     private Editorial editorial;
     private Categoria categoria;
     private Genero genero;
     private Idioma idioma;
     private List<Autor> autores;
+    private Propuesta propuestaOrigen;
 
     //Constructores☆゜・。。・゜゜・。。・゜★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★
     public Libro() {
     }
 
-    public Libro(String isbn, String titulo, String sinopsis, int cantidadPaginas, double precio, LocalDateTime fechaPublicacion, Editorial editorial, Categoria categoria, Genero genero, Idioma idioma, List<Autor> autores) {
+    public Libro(String isbn, String titulo, String sinopsis, int cantidadPaginas, BigDecimal precio, LocalDate fechaPublicacion, int cantidadDisponible, Editorial editorial, Categoria categoria, Genero genero, Idioma idioma, List<Autor> autores, Propuesta propuestaOrigen) {
         this.isbn = isbn;
         this.titulo = titulo;
         this.sinopsis = sinopsis;
         this.cantidadPaginas = cantidadPaginas;
         this.precio = precio;
         this.fechaPublicacion = fechaPublicacion;
+        this.cantidadDisponible = cantidadDisponible;
+        this.stockMinimo = 5;
         this.editorial = editorial;
         this.categoria = categoria;
         this.genero = genero;
         this.idioma = idioma;
-        this.autores = new ArrayList<>();
+        this.autores = autores;
+        this.propuestaOrigen = propuestaOrigen;
     }
 
-    public Libro(int id, String isbn, String titulo, String sinopsis, int cantidadPaginas, double precio, LocalDateTime fechaPublicacion, Editorial editorial, Categoria categoria, Genero genero, Idioma idioma, List<Autor> autores) {
+    public Libro(int id, String isbn, String titulo, String sinopsis, int cantidadPaginas, BigDecimal precio, LocalDate fechaPublicacion, int cantidadDisponible, Editorial editorial, Categoria categoria, Genero genero, Idioma idioma, List<Autor> autores, Propuesta propuestaOrigen) {
         this.id = id;
         this.isbn = isbn;
         this.titulo = titulo;
@@ -45,16 +51,23 @@ public class Libro {
         this.cantidadPaginas = cantidadPaginas;
         this.precio = precio;
         this.fechaPublicacion = fechaPublicacion;
+        this.cantidadDisponible = cantidadDisponible;
+        this.stockMinimo = 5;
         this.editorial = editorial;
         this.categoria = categoria;
         this.genero = genero;
         this.idioma = idioma;
-        this.autores = new ArrayList<>();
+        this.autores = autores;
+        this.propuestaOrigen = propuestaOrigen;
     }
 
     //Getters y setters☆゜・。。・゜゜・。。・゜★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getIsbn() {
@@ -89,20 +102,36 @@ public class Libro {
         this.cantidadPaginas = cantidadPaginas;
     }
 
-    public double getPrecio() {
+    public BigDecimal getPrecio() {
         return precio;
     }
 
-    public void setPrecio(double precio) {
+    public void setPrecio(BigDecimal precio) {
         this.precio = precio;
     }
 
-    public LocalDateTime getFechaPublicacion() {
+    public LocalDate getFechaPublicacion() {
         return fechaPublicacion;
     }
 
-    public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
         this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public int getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public void setStockMinimo(int stockMinimo) {
+        this.stockMinimo = stockMinimo;
     }
 
     public Editorial getEditorial() {
@@ -145,21 +174,40 @@ public class Libro {
         this.autores = autores;
     }
 
+    public Propuesta getPropuestaOrigen() {
+        return propuestaOrigen;
+    }
+
+    public void setPropuestaOrigen(Propuesta propuestaOrigen) {
+        this.propuestaOrigen = propuestaOrigen;
+    }
+
+    //Métodos☆゜・。。・゜゜・。。・゜★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★
+    public boolean estaDisponible() {
+        return  cantidadDisponible > 0;
+    }
+
+    public boolean necesitaReposicion() {
+        return cantidadDisponible <= stockMinimo;
+    }
+
     //ToString☆゜・。。・゜゜・。。・゜★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★
     @Override
     public String toString() {
         return "Libro: " +
                 "\nIsbn: " + isbn +
-                "\nTitulo: " + titulo +
+                "\nTítulo: " + titulo +
                 "\nSinopsis: " + sinopsis +
                 "\nCantidad de páginas: " + cantidadPaginas +
                 "\nPrecio: " + precio +
-                "\nFecha de publicación: " + fechaPublicacion +
-                "\nEditorial: " + editorial.getNombre() +
-                "\nCategoría: " + categoria.getNombre() +
-                "\nGénero: " + genero.getNombre() +
-                "\nIdioma: " + idioma.getNombre() +
-                "\nAutores: " + autores;
+                "\nFecha publicación: " + fechaPublicacion +
+                "\nCantidad disponible: " + cantidadDisponible +
+                "\nEditorial: " + editorial +
+                "\nCategoría: " + categoria +
+                "\nGénero: " + genero +
+                "\nIdioma: " + idioma +
+                "\nAutores: " + autores +
+                "\nPropuesta origen: " + propuestaOrigen;
     }
 
 }

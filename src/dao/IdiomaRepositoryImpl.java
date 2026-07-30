b87f2af5cb24e32c90206ba431029c01259cbdcb
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-07-29 21:34:02 -0300

    feat(dao): crear implementación inicial de IdiomaRepository
    - Implementar el método insertar
    - Agregar la estructura base de los métodos restantes

---
+++ src/dao/IdiomaRepositoryImpl.java
@@ -0,0 +1,64 @@
+package dao;
+
+import model.Idioma;
+import repository.IdiomaRepository;
+
+import java.sql.*;
+import java.util.List;
+
+public class IdiomaRepositoryImpl extends RepositoryBase<Idioma> implements IdiomaRepository {
+
+    //Conexión declarada en la clase padre(RepositoryBase)
+
+    @Override
+    public void insertar(Idioma idioma) {
+        String sql = "INSERT INTO idioma (nombre) VALUES (?)";
+
+        // Preparo la consulta y le pido a la BD que me devulva el id que genere automáticamente
+        try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, idioma.getNombre());
+            ps.executeUpdate(); // Ejecuto el insert
+
+            // Solicito el id que generó el insert
+            try(ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                // Si la BD me devuelve un id nuevo, lo extraigo y lo asigno a mi objeto
+                if (generatedKeys.next()) {
+                    idioma.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("No se pudo insertar el idioma", e);
+        }
+    }
+
+    @Override
+    public void actualizar(Idioma idioma) {
+
+    }
+
+    @Override
+    public void eliminar(Idioma idioma) {
+
+    }
+
+    @Override
+    public Idioma buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Idioma> listarTodos() {
+        return null;
+    }
+
+    @Override
+    public boolean existeNombre(String nombre) {
+        return false;
+    }
+
+    @Override
+    protected Idioma mapear(ResultSet rs) throws SQLException {
+        return null;
+    }
+
+}

02c0a1c78718974e2c9d735b9b33c90d2582bc98
Ana Gabriela <ana.medina@davinci.edu.ar>
2026-08-01 02:30:21 -0300

    feat(dao): implemetar inserción, actualización y eliminación de detalles de ventas

--- src/dao/DetalleVentaRepositoryImpl.java
+++ src/dao/DetalleVentaRepositoryImpl.java
@@ -30,18 +30,53 @@ public class DetalleVentaRepositoryImpl extends RepositoryBase<DetalleVenta> imp
     }
 
     @Override
-    public void insertar(DetalleVenta entidad) {
+    public void insertar(DetalleVenta detalleVenta) {
+        String sql = "INSERT INTO vente_libro (fk_id_venta, fk_id_libro, cantidad_vendida, precio_unitario) VALUES (?, ?, ?, ?)"; // Tabla intermedia entre venta y libro en la BD
 
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, detalleVenta.getVenta().getId());
+            ps.setInt(2, detalleVenta.getLibro().getId());
+            ps.setInt(3, detalleVenta.getCantidadVendida());
+            ps.setBigDecimal(4, detalleVenta.getPrecioUnitario());
+
+            ps.executeUpdate();
+
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    detalleVenta.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("No se pudo insertar los detalles de la venta", e);
+        }
     }
 
     @Override
-    public void actualizar(DetalleVenta entidad) {
+    public void actualizar(DetalleVenta detalleVenta) {
+        String sql = "UPDATE venta_libro SET cantidad_vendida = ?, precio_unitario = ? WHERE id_venta_libro = ?";
 
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, detalleVenta.getCantidadVendida());
+            ps.setBigDecimal(2, detalleVenta.getPrecioUnitario());
+            ps.setInt(3, detalleVenta.getId());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("No se pudo actualizar el detalle de la venta", e)
+        }
     }
 
     @Override
-    public void eliminar(DetalleVenta entidad) {
+    public void eliminar(DetalleVenta detalleVenta) {
+        String sql = "DELETE FROM venta_libro WHERE id_venta_libro = ?";
 
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, detalleVenta.getId());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("No se pudo eliminar el detalle de la venta", e);
+        }
     }
 
     @Override
